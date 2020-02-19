ca69597282fc88263188c8009b9a2ff63530e10c
limbo-yan <46770483+limbo-yan@users.noreply.github.com>
2020-02-19 18:36:19 -0500

    Add files via upload

---
+++ guessing_game.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+	/*
+		TODO:
+		change this guessing game to make it accept command line arguement
+		or accept user input so that it can generate a random secret number
+		within user specified range.
+		Google rand() and srand()
+	*/
+	//////////Your code here//////////
+	printf("Type your upper range here:\n ");
+	int max = 0;
+	scanf("%d",&max);
+	printf("Type you lower range here:\n ");
+	int min = 0;
+	scanf("%d",&min);
+	int secret = (rand() % (max + 1 - min)) + min;
+	//////////End of the code/////////
+
+	printf("Welcome to my guessing game!\n");
+	while(true) {
+		// Print a prompt (part of a line)
+		printf("Type your guess and then ENTER: ");
+		// Make sure it becomes visible (otherwise C is allowed to wait
+		// until you print a new line, e.g., '\n' before doing anything.
+		fflush(stdout); // https://www.geeksforgeeks.org/use-fflushstdin-c/
+		int guess = 0;
+		int status = scanf("%d", &guess);
+		if (status != 1) break;
+			printf("You guessed: %d\n", guess);
+		if (secret == guess) {
+			printf("Congratulations! You guessed the secret.\n");
+			break;
+		} else if (secret < guess) {
+			printf("Too high! Guess again.\n");
+		} else {
+			printf("Too low! Guess again.\n");
+		}
+	}
+}
